1cfdd9ae7e5da23d5371e06efb50acc240233792
Nikita <lyapin2010@yandex.ru>
2021-12-14 18:38:14 +0300

    git_ignore

--- kalman_filter/kalman_filter/kalman_filter.cpp
+++ kalman_filter/kalman_filter/kalman_filter.cpp
@@ -1,12 +1,157 @@
-﻿#include <iostream>
-#include "Eigen"
-#include <math.h>
-#include <fstream>
-#include <string>
-#define filename "book2.csv"
-# define M_PI 3.14159265358979323846
+﻿#include "library.h"
 
-Eigen::MatrixXd getB(double teta, double phi, double psi, double deltateta, double deltaphi, double deltapsi, double vx, double vy, double vz, double wx, double wy, double wz, double dt)
+class my_ekf
+{
+    Eigen::MatrixXd A_k_minus_1;
+    Eigen::VectorXd process_noise_v_k_minus_1;
+    Eigen::VectorXd k1;
+    Eigen::MatrixXd Q_k;
+    Eigen::MatrixXd H_k;
+    Eigen::VectorXd k;
+    Eigen::VectorXd v;
+    Eigen::VectorXd omega;
+    Eigen::VectorXd temp;
+    Eigen::MatrixXd R_k;
+    Eigen::VectorXd sensor_noise_w_k;
+    Eigen::VectorXd state_estimate_k_minus_1;
+    Eigen::VectorXd k2;
+    Eigen::VectorXd control_vector_k_minus_1;
+    Eigen::MatrixXd P_k_minus_1;
+
+    public:
+
+        my_ekf(Eigen::MatrixXd A_k_minus_1, Eigen::VectorXd process_noise_v_k_minus_1, Eigen::VectorXd k1, Eigen::MatrixXd H_k, Eigen::VectorXd k, 
+               Eigen::VectorXd sensor_noise_w_k, Eigen::VectorXd state_estimate_k_minus_1, 
+               Eigen::VectorXd k2, Eigen::VectorXd control_vector_k_minus_1)
+        {
+            this->A_k_minus_1 = A_k_minus_1;
+            this->process_noise_v_k_minus_1 = process_noise_v_k_minus_1;
+            this->Q_k = k1.asDiagonal();
+            this->H_k = H_k;
+            this->R_k = k.asDiagonal();
+            this->sensor_noise_w_k = sensor_noise_w_k;
+            this->state_estimate_k_minus_1 = state_estimate_k_minus_1;
+            this->P_k_minus_1 = k2.asDiagonal();
+            this->control_vector_k_minus_1 = control_vector_k_minus_1;
+            
+        }
+
+    Eigen::MatrixXd getB(double teta, double phi, double psi, double deltateta, double deltaphi, double deltapsi, double vx, double vy, double vz, 
+                         double wx, double wy, double wz)
+    {
+        Eigen::MatrixXd B(12, 6);
+        B <<
+            0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0,
+            (cos(teta) * cos(phi))* vx, (-sin(psi) * cos(teta))* vy, sin(teta)* vz, 0, 0, 0,
+            (sin(psi) * cos(phi) + cos(psi) * sin(teta) * sin(phi))* vx, (cos(psi) * cos(phi) - sin(psi) * sin(teta) * sin(phi))* vy, (-cos(teta) * sin(phi))* vz, 0, 0, 0,
+            (sin(psi) * sin(phi) - cos(psi) * sin(teta) * cos(phi))* vx, (cos(psi) * sin(phi) + sin(psi) * sin(teta) * cos(phi))* vy, (cos(teta) * cos(phi))* vz, 0, 0, 0,
+            0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0,
+            0, 0, 0, (cos(deltateta) * cos(deltaphi))* wx, (-sin(deltapsi) * cos(deltateta))* wy, sin(deltateta)* wz,
+            0, 0, 0, (sin(deltapsi) * cos(deltaphi) + cos(deltapsi) * sin(deltateta) * sin(deltaphi))* wx, (cos(deltapsi) * cos(deltaphi) - sin(deltapsi) * sin(deltateta) * sin(deltaphi))* wy, -cos(deltateta) * sin(deltaphi) * wz,
+            0, 0, 0, (sin(deltapsi) * sin(deltaphi) - cos(deltapsi) * sin(deltateta) * cos(deltaphi))* wx, (cos(deltapsi) * sin(deltaphi) + sin(deltapsi) * sin(deltateta) * cos(deltaphi))* wy, cos(deltateta)* cos(deltaphi)* wz;
+        return B;
+    }
+
+    int ekf(Eigen::VectorXd z_k_observation_vector, Eigen::VectorXd state_estimate_k_minus_1, Eigen::VectorXd control_vector_k_minus_1,
+            Eigen::MatrixXd P_k_minus_1, Eigen::VectorXd d_k, Eigen::MatrixXd A_k_minus_1, Eigen::VectorXd process_noise_v_k_minus_1, Eigen::MatrixXd Q_k, 
+            Eigen::VectorXd sensor_noise_w_k, Eigen::MatrixXd H_k, Eigen::MatrixXd R_k, Eigen::VectorXd &state_estimate_k, Eigen::MatrixXd&P_k)
+    {
+        Eigen::VectorXd measurement_residual_y_k(9);
+        Eigen::MatrixXd S_k(9, 9);
+        Eigen::MatrixXd K_k(12, 9);
+       
+        state_estimate_k =  A_k_minus_1 * (state_estimate_k_minus_1)+ getB(state_estimate_k_minus_1(7), state_estimate_k_minus_1(6), 
+                            state_estimate_k_minus_1(8), d_k(0), d_k(1), d_k(2), state_estimate_k_minus_1(3), state_estimate_k_minus_1(4), 
+                            state_estimate_k_minus_1(5), state_estimate_k_minus_1(9), state_estimate_k_minus_1(10), 
+                            state_estimate_k_minus_1(11)) * (control_vector_k_minus_1)+(process_noise_v_k_minus_1);
+        std::cout <<"before: " << state_estimate_k.transpose() << std::endl;
+        P_k = A_k_minus_1 * P_k_minus_1 * A_k_minus_1.transpose() + Q_k;
+        
+        measurement_residual_y_k = z_k_observation_vector - ((H_k * state_estimate_k) + (sensor_noise_w_k));
+        
+        S_k = H_k * P_k * H_k.transpose() + R_k;
+        K_k = P_k * H_k.transpose() * S_k.inverse();
+        state_estimate_k = state_estimate_k + (K_k * measurement_residual_y_k);
+        P_k = P_k - (K_k * H_k * P_k);
+        std::cout << "after: " << state_estimate_k.transpose()<<std::endl;
+        return 0;
+
+    }
+
+    void process(Eigen::VectorXd z_k_observation_vector, Eigen::VectorXd d_k, int l, Eigen::MatrixXd&rez)
+    {
+        Eigen::VectorXd state_estimate_k(12);
+        Eigen::MatrixXd P_k(12, 12);
+        my_ekf::ekf(z_k_observation_vector, state_estimate_k_minus_1, control_vector_k_minus_1, P_k_minus_1, d_k, 
+                    A_k_minus_1, process_noise_v_k_minus_1, Q_k, sensor_noise_w_k, H_k, R_k, state_estimate_k, P_k);
+        rez.col(l) = state_estimate_k;
+        state_estimate_k_minus_1 = state_estimate_k;
+        P_k_minus_1 = P_k;
+       
+    }
+};
+
+int main()
+{
+    double dt(1.0);
+    Eigen::MatrixXd A_k_minus_1(12, 12);
+    Eigen::VectorXd process_noise_v_k_minus_1(12);
+    Eigen::VectorXd k1(12);
+    Eigen::MatrixXd H_k(9, 12);
+    Eigen::VectorXd k(9);
+    Eigen::VectorXd sensor_noise_w_k(9);
+    Eigen::VectorXd v(3);
+    Eigen::VectorXd omega(3);
+    Eigen::VectorXd state_estimate_k_minus_1(12);
+    Eigen::VectorXd control_vector_k_minus_1(6);
+    Eigen::VectorXd k2(12);
+    Eigen::VectorXd temp(12);
+    Eigen::VectorXd z_k_observation_vector(9);
+    Eigen::VectorXd d_k(3);
+    Eigen::MatrixXd rez(12, 298);
+
+    temp << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+    mat_set(A_k_minus_1, process_noise_v_k_minus_1, k1, H_k, k, sensor_noise_w_k, state_estimate_k_minus_1, control_vector_k_minus_1, 
+               k2, dt);
+   
+    my_ekf ekf1(A_k_minus_1, process_noise_v_k_minus_1, k1, H_k, k, sensor_noise_w_k, state_estimate_k_minus_1, k2,
+                control_vector_k_minus_1);
+
+    std::ofstream myfile;
+    myfile.open("C:\\Nikita\\2021\\704\\inter_drone\\kalman_filter\\kalman_filter\\result.csv");
+    myfile << "x; y; z; vx; vy; vz; phi; teta; psi; omegaX; omegaY; omegaZ\n";
+    std::ifstream work_file("C:\\Nikita\\2021\\704\\inter_drone\\kalman_filter\\kalman_filter\\book2.csv");
+    std::string line;
+    //char delimiter = ';';
+    std::getline(work_file, line);
+    std::getline(work_file, line);
+    rez.col(0) = temp;
+    myfile << std::to_string(rez(0, 0)) + ";" << std::to_string(rez(1, 0)) + ";" << std::to_string(rez(2, 0)) + ";" << std::to_string(rez(3, 0)) + ";" <<
+        std::to_string(rez(4, 0)) + ";" << std::to_string(rez(5, 0)) + ";" << std::to_string(rez(6, 0)) + ";" << std::to_string(rez(7, 0)) + ";" <<
+        std::to_string(rez(8, 0)) + ";" << std::to_string(rez(9, 0)) + ";" << std::to_string(rez(10, 0)) + ";" << std::to_string(rez(11, 0)) + "\n";
+
+    while (getline(work_file, line))
+    {
+        int l = 1;
+        set_z(line, z_k_observation_vector, d_k);
+        ekf1.process(z_k_observation_vector, d_k, l, rez);
+        myfile << std::to_string(rez(0, l)) + ";" << std::to_string(rez(1, l)) + ";" << std::to_string(rez(2, l)) + ";" << std::to_string(rez(3, l)) + ";" <<
+            std::to_string(rez(4, l)) + ";" << std::to_string(rez(5, l)) + ";" << std::to_string(rez(6, l) * 180 / M_PI) + ";" << std::to_string(rez(7, l) * 180 / M_PI) + ";" <<
+            std::to_string(rez(8, l) * 180 / M_PI) + ";" << std::to_string(rez(9, l)) + ";" << std::to_string(rez(10, l)) + ";" << std::to_string(rez(11, l)) + "\n";
+        l += 1;
+        
+    }
+    work_file.close();
+    myfile.close();
+}
+
+
+
+/*Eigen::MatrixXd getB(double teta, double phi, double psi, double deltateta, double deltaphi, double deltapsi, double vx, double vy, double vz, double wx, double wy, double wz, double dt)
 {
     Eigen::MatrixXd B(12, 6);
     B <<
@@ -180,3 +325,4 @@ int main()
 
 
 }
+*/
